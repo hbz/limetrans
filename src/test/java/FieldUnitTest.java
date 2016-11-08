b7b8574e7dbeac19aff04a72d9c4b58477901f2c
Philipp von Böselager <philipp.boeselager@web.de>
2016-11-08 17:55:20 +0100

    Setup metafacture test framework - part 1

---
+++ src/test/java/FieldUnitTest.java
@@ -0,0 +1,6 @@
+import org.culturegraph.mf.test.*;
+import org.junit.runner.RunWith;
+
+@RunWith(MetamorphTestSuite.class)
+@MetamorphTestSuite.TestDefinitions({"Field001Test.xml"})
+public final class FieldUnitTest {}
