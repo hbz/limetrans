7baf30a7f45ec120d945b3cbaeece1bd84b665a0
Jens Wille <jens.wille@gmail.com>
2016-11-10 18:19:00 +0100

    Reorganize test and resource files.
    Split transformation file for client (ex: DE-836) into (potentially reusable) macro and map definition files and (client-specific) rule file(s).
    
    Dropped empty transformation file.
    
    Renamed test definition file for client.
    
    Introduced ISIL entity for client ID.
    
    Gave input and output files for test case more descriptive names.

--- src/test/java/FieldUnitTest.java
+++ src/test/java/FieldUnitTest.java
@@ -1,5 +1,7 @@
 import org.culturegraph.mf.test.MetamorphTestSuite;
+import org.culturegraph.mf.test.MetamorphTestSuite.TestDefinitions;
 import org.junit.runner.RunWith;
 
 @RunWith(MetamorphTestSuite.class)
+@TestDefinitions({"DE-836.xml"})
 public final class FieldUnitTest {}
