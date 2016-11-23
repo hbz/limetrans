478b110989aa7262abf787737c5a575170ecfb79
Jens Wille <jens.wille@gmail.com>
2016-11-23 08:37:34 +0100

    Move transformation tests to dedicated subdirectory.

--- src/test/java/transformation/FieldUnitTest.java
+++ src/test/java/transformation/FieldUnitTest.java
@@ -3,5 +3,5 @@ import org.culturegraph.mf.test.MetamorphTestSuite.TestDefinitions;
 import org.junit.runner.RunWith;
 
 @RunWith(MetamorphTestSuite.class)
-@TestDefinitions({"DE-836.xml"})
+@TestDefinitions({"transformation/DE-836.xml"})
 public final class FieldUnitTest {}
