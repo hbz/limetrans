4c78cf182b74ffaa4bd3883ce766e6b84f36a998
Jens Wille <jens.wille@gmail.com>
2016-12-20 10:41:13 +0100

    Add Helpers.getResource() and slurpFile() variants.

--- src/main/java/hbz/limetrans/util/Helpers.java
+++ src/main/java/hbz/limetrans/util/Helpers.java
@@ -5,11 +5,11 @@ import org.xbib.common.settings.Settings;
 import org.xbib.common.settings.loader.SettingsLoader;
 import org.xbib.common.settings.loader.SettingsLoaderFactory;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 public class Helpers {
 
@@ -17,15 +17,33 @@ public class Helpers {
         final SettingsLoader settingsLoader = SettingsLoaderFactory.loaderFromResource(aUrl.toString());
 
         final Settings settings = Settings.settingsBuilder()
-            .put(settingsLoader.load(IOUtils.toString(aUrl, Charset.defaultCharset())))
+            .put(settingsLoader.load(slurpFile(aUrl)))
             .replacePropertyPlaceholders()
             .build();
 
         return settings;
     }
 
+    public static String slurpFile(final URL aUrl) throws IOException {
+        return IOUtils.toString(aUrl, Charset.defaultCharset());
+    }
+
     public static String slurpFile(final String aPath) throws IOException {
-        return new String(Files.readAllBytes(Paths.get(aPath)));
+        return slurpFile(new File(aPath).toURI().toURL());
+    }
+
+    public static String slurpFile(final String aPath, final Class aClass) throws IOException {
+        return slurpFile(getResource(aClass, aPath));
+    }
+
+    public static URL getResource(final Class aClass, final String aPath) throws IOException {
+        final URL url = aClass.getResource(aPath);
+        if (url == null) {
+            throw new FileNotFoundException("Resource not found for " + aClass.toString() + ": " + aPath);
+        }
+        else {
+            return url;
+        }
     }
 
 }
