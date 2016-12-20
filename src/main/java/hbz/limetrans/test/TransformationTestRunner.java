509882c3e5937e847dcd874186c320784e5da124
Jens Wille <jens.wille@gmail.com>
2016-12-20 10:42:03 +0100

    Use resource loader in TransformationTest{Runner,Suite}.

--- src/main/java/hbz/limetrans/test/TransformationTestRunner.java
+++ src/main/java/hbz/limetrans/test/TransformationTestRunner.java
@@ -1,36 +1,45 @@
 package hbz.limetrans.test;
 
+import hbz.limetrans.util.Helpers;
+
 import org.junit.runner.Description;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.ParentRunner;
 import org.junit.runners.model.InitializationError;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransformationTestRunner extends ParentRunner<TransformationTestCase> {
 
     public static final String FILE_EXT = ".xml";
-    public static final String RULES_PATH = "src/main/resources/transformation/%s.xml";
+    public static final String RULES_PATH = TransformationTestSuite.ROOT_PATH + "/%s.xml";
 
     private final List<TransformationTestCase> mTestCases;
     private final String mName;
-    private final String mRulesPath;
 
-    TransformationTestRunner(final Class<?> aClass, final File aDirectory) throws InitializationError {
+    TransformationTestRunner(final Class aClass, final File aDirectory) throws InitializationError {
         super(aClass);
 
         mName = aDirectory.getName();
-        mRulesPath = String.format(RULES_PATH, mName);
-
         mTestCases = new ArrayList<>();
+
+        final URL rulesUrl;
+        try {
+            rulesUrl = Helpers.getResource(aClass, String.format(RULES_PATH, mName));
+        }
+        catch (final IOException e) {
+            throw new InitializationError(e);
+        }
 
         for (final File file : aDirectory.listFiles()) {
             final String fileName = file.toString();
 
             if (fileName.endsWith(FILE_EXT)) {
-                mTestCases.add(new TransformationTestCase(mRulesPath, fileName));
+                mTestCases.add(new TransformationTestCase(rulesUrl.getPath(), fileName));
             }
         }
 
--- src/main/java/hbz/limetrans/test/TransformationTestSuite.java
+++ src/main/java/hbz/limetrans/test/TransformationTestSuite.java
@@ -1,5 +1,7 @@
 package hbz.limetrans.test;
 
+import hbz.limetrans.util.Helpers;
+
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
 import org.junit.runner.notification.RunNotifier;
@@ -7,21 +9,31 @@ import org.junit.runners.ParentRunner;
 import org.junit.runners.model.InitializationError;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
 public class TransformationTestSuite extends ParentRunner<Runner> {
 
-    public static final String ROOT_PATH = "src/test/resources/transformation";
+    public static final String ROOT_PATH = "/transformation";
 
     private final List<Runner> mRunners;
 
-    public TransformationTestSuite(final Class<?> aClass) throws InitializationError {
+    public TransformationTestSuite(final Class aClass) throws InitializationError {
         super(aClass);
 
         mRunners = new ArrayList<>();
 
-        for (final File directory : new File(ROOT_PATH).listFiles()) {
+        final URL rootUrl;
+        try {
+            rootUrl = Helpers.getResource(aClass, ROOT_PATH);
+        }
+        catch (final IOException e) {
+            throw new InitializationError(e);
+        }
+
+        for (final File directory : new File(rootUrl.getPath()).listFiles()) {
             mRunners.add(new TransformationTestRunner(aClass, directory));
         }
 
