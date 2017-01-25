71332718192fdb1b11f4b3cc38a7dfc119f45a5c
Jens Wille <jens.wille@gmail.com>
2017-01-25 13:36:59 +0100

    SonarQube: Immediately return this expression instead of assigning it to the temporary variable "settings".
    https://sonarqube.com/issues/search#issues=AVnVB0k-Ybr9ugKetHWl

--- src/main/java/hbz/limetrans/util/Helpers.java
+++ src/main/java/hbz/limetrans/util/Helpers.java
@@ -22,12 +22,10 @@ public class Helpers {
     public static Settings loadSettings(final URL aUrl) throws IOException {
         final SettingsLoader settingsLoader = SettingsLoaderFactory.loaderFromResource(aUrl.toString());
 
-        final Settings settings = Settings.settingsBuilder()
+        return Settings.settingsBuilder()
             .put(settingsLoader.load(slurpFile(aUrl)))
             .replacePropertyPlaceholders()
             .build();
-
-        return settings;
     }
 
     public static Settings loadSettings(final File aFile) throws IOException {
