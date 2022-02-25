bd18d8565d5beab164881b5617c95664805ba651
Jens Wille <jens.wille@hbz-nrw.de>
2022-02-25 12:19:12 +0100

    Print final memory (like Maven) and add optional memory logging.
    Define system property hbz.limetrans.memlog[=<interval>] to enable.

--- src/main/java/hbz/limetrans/Main.java
+++ src/main/java/hbz/limetrans/Main.java
@@ -7,7 +7,15 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.lang.management.MemoryMXBean;
+import java.lang.management.MemoryUsage;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
+import java.util.concurrent.atomic.LongAccumulator;
+import java.util.concurrent.atomic.LongAdder;
 import java.util.function.Consumer;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -15,6 +23,12 @@ import java.util.regex.Pattern;
 public final class Main {
 
     private static final Logger LOGGER = LogManager.getLogger();
+
+    private static final String MEMLOG = "memlog";
+
+    private static final long MS = 1000;
+    private static final long KB = 1024;
+    private static final long MB = KB * KB;
 
     private static final String[] INDEX_SETTING = new String[]{"output", "elasticsearch", "index"};
 
@@ -103,7 +117,17 @@ public final class Main {
     }
 
     public static void main(final String[] aArgs) throws IOException {
+        final Integer interval = Helpers.getProperty(MEMLOG, Integer::parseInt, 10);
+        final MemLog memlog = interval != null ? new MemLog(interval) : null;
+
         new Limetrans(setup(aArgs)).process();
+
+        final Long rss = MemLog.getRss();
+        final MemoryUsage heap = MemLog.getHeap();
+        final MemoryUsage nonHeap = MemLog.getNonHeap();
+
+        LOGGER.info("Final Memory: {}M/{}M" + (rss != null ? String.format(" (%dM)", rss) : "") + (memlog != null ? String.format(" [%s]", memlog) : ""),
+                (heap.getUsed() + nonHeap.getUsed()) / MB, (heap.getCommitted() + nonHeap.getCommitted()) / MB);
     }
 
     private static Settings setup(final String[] aArgs) throws IOException {
@@ -126,6 +150,118 @@ public final class Main {
     private interface VarargsOperator<T> {
         @SuppressWarnings("unchecked")
         T[] apply(T... aArgs);
+    }
+
+    private static class MemLog {
+
+        private static final MemoryMXBean MBEAN = ManagementFactory.getMemoryMXBean();
+
+        private static final Path STATUS = Paths.get("/proc/self/status");
+        private static final Pattern RSS_PATTERN = Pattern.compile("\\AVmRSS:\\s+(\\d+)\\s+kB");
+
+        private final LongAccumulator mMaxCommitted = new LongAccumulator(Long::max, Long.MIN_VALUE);
+        private final LongAccumulator mMaxRss = new LongAccumulator(Long::max, Long.MIN_VALUE);
+        private final LongAccumulator mMaxUsed = new LongAccumulator(Long::max, Long.MIN_VALUE);
+        private final LongAdder mCount = new LongAdder();
+        private final LongAdder mTotalCommitted = new LongAdder();
+        private final LongAdder mTotalRss = new LongAdder();
+        private final LongAdder mTotalUsed = new LongAdder();
+        private final Thread mThread;
+
+        private MemLog(final int aInterval) {
+            mThread = new Thread(() -> {
+                while (true) {
+                    mCount.increment();
+
+                    final MemoryUsage heap = getHeap();
+                    final MemoryUsage nonHeap = getNonHeap();
+
+                    final long heapUsed = heap.getUsed() / MB;
+                    final long nonHeapUsed = nonHeap.getUsed() / MB;
+
+                    final long heapCommitted = heap.getCommitted() / MB;
+                    final long nonHeapCommitted = nonHeap.getCommitted() / MB;
+
+                    final long used = heapUsed + nonHeapUsed;
+                    mMaxUsed.accumulate(used);
+                    mTotalUsed.add(used);
+
+                    final long committed = heapCommitted + nonHeapCommitted;
+                    mMaxCommitted.accumulate(committed);
+                    mTotalCommitted.add(committed);
+
+                    final StringBuilder sb = new StringBuilder(String.format(
+                                "Current Memory: heap.used=%dM, nonheap.used=%dM, heap.committed=%dM, nonheap.committed=%dM",
+                                heapUsed, nonHeapUsed, heapCommitted, nonHeapCommitted));
+
+                    final Long rss = getRss();
+
+                    if (rss != null) {
+                        mMaxRss.accumulate(rss);
+                        mTotalRss.add(rss);
+
+                        sb.append(String.format(", rss=%dM", rss));
+                    }
+
+                    LOGGER.info(sb);
+
+                    if (Thread.interrupted()) {
+                        break;
+                    }
+                    else {
+                        try {
+                            Thread.sleep(aInterval * MS);
+                        }
+                        catch (final InterruptedException e) {
+                            break;
+                        }
+                    }
+                }
+            }, MEMLOG);
+
+            mThread.start();
+        }
+
+        private static MemoryUsage getHeap() {
+            return MBEAN.getHeapMemoryUsage();
+        }
+
+        private static MemoryUsage getNonHeap() {
+            return MBEAN.getNonHeapMemoryUsage();
+        }
+
+        private static Long getRss() {
+            try {
+                for (final String line : Files.readAllLines(STATUS)) {
+                    final Matcher matcher = RSS_PATTERN.matcher(line);
+
+                    if (matcher.matches()) {
+                        return Long.parseLong(matcher.group(1)) / KB;
+                    }
+                }
+            }
+            catch (final IOException e) {
+            }
+
+            return null;
+        }
+
+        @Override
+        public String toString() {
+            if (mThread.isAlive()) {
+                mThread.interrupt();
+
+                try {
+                    mThread.join(MS);
+                }
+                catch (final InterruptedException e) {
+                }
+            }
+
+            return String.format("used.max=%dM, used.avg=%dM, committed.max=%dM, committed.avg=%dM, rss.max=%dM, rss.avg=%dM",
+                    mMaxUsed.get(), mTotalUsed.sum() / mCount.sum(), mMaxCommitted.get(), mTotalCommitted.sum() / mCount.sum(), mMaxRss.get(), mTotalRss.sum() / mCount.sum());
+        }
+
     }
 
 }
