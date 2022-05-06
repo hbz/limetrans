5282d3dc0ebd014645c2d2cdbd7a0f136d6ed418
Jens Wille <jens.wille@hbz-nrw.de>
2022-05-06 11:54:27 +0200

    Perform Alma Fix setup (e.g., maps) only once, not repeatedly for each and every record.

---
+++ src/main/java/hbz/limetrans/function/Once.java
@@ -0,0 +1,26 @@
+package hbz.limetrans.function;
+
+import org.metafacture.metafix.Metafix;
+import org.metafacture.metafix.Record;
+import org.metafacture.metafix.RecordTransformer;
+import org.metafacture.metafix.api.FixContext;
+
+import java.util.List;
+import java.util.Map;
+
+public class Once implements FixContext {
+
+    private boolean executed;
+
+    public Once() {
+    }
+
+    @Override
+    public void execute(final Metafix metafix, final Record record, final List<String> params, final Map<String, String> options, final RecordTransformer recordTransformer) {
+        if (!executed) {
+            executed = true;
+            recordTransformer.transform(record);
+        }
+    }
+
+}
