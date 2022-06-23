55da0b6c1a706854637a755caa94e27aacfa8a6e
Jens Wille <jens.wille@hbz-nrw.de>
2022-06-23 13:09:31 +0200

    Add custom Fix function to drop repeated fields. (c4576ab)

---
+++ src/main/java/hbz/limetrans/function/DropRepeated.java
@@ -0,0 +1,27 @@
+package hbz.limetrans.function;
+
+import org.metafacture.metafix.Metafix;
+import org.metafacture.metafix.Record;
+import org.metafacture.metafix.Value;
+import org.metafacture.metafix.api.FixFunction;
+
+import java.util.List;
+import java.util.Map;
+
+public class DropRepeated implements FixFunction {
+
+    public DropRepeated() {
+    }
+
+    @Override
+    public void apply(final Metafix aMetafix, final Record aRecord, final List<String> aParams, final Map<String, String> aOptions) {
+        aParams.forEach(field -> {
+            final Value value = aRecord.get(field);
+
+            if (value != null) {
+                value.matchType().ifArray(a -> aRecord.set(field, a.get(0)));
+            }
+        });
+    }
+
+}
