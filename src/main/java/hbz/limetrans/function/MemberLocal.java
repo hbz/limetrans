7e232def61275e35bb61c5f580c60b5e385f4a31
Jens Wille <jens.wille@hbz-nrw.de>
2022-09-08 11:43:34 +0200

    Include local Alma fields for local records. [DOX-1532]
    Local records don't have subfield `M`.

---
+++ src/main/java/hbz/limetrans/function/MemberLocal.java
@@ -0,0 +1,33 @@
+package hbz.limetrans.function;
+
+import org.metafacture.metafix.FixConditional;
+import org.metafacture.metafix.Metafix;
+import org.metafacture.metafix.Record;
+import org.metafacture.metafix.api.FixPredicate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class MemberLocal implements FixPredicate {
+
+    private static final String SUBFIELD = "M";
+
+    public MemberLocal() {
+    }
+
+    @Override
+    public boolean test(final Metafix aMetafix, final Record aRecord, final List<String> aParams, final Map<String, String> aOptions) {
+        final List<String> params = new ArrayList<>();
+        params.add(aParams.get(0) + "." + SUBFIELD);
+
+        if (!FixConditional.exists.test(aMetafix, aRecord, params, null)) {
+            return true;
+        }
+        else {
+            params.add(aMetafix.getVars().get("member"));
+            return FixConditional.any_equal.test(aMetafix, aRecord, params, null);
+        }
+    }
+
+}
