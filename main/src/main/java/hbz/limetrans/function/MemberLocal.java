8fa2e1bd8eee8b93e2db684fac54a1d5b9aac210
Jens Wille <jens.wille@hbz-nrw.de>
2025-09-05 11:17:55 +0200

    Adapt for Fix command enums as individual classes. (metafacture/metafacture-core#706)

--- main/src/main/java/hbz/limetrans/function/MemberLocal.java
+++ main/src/main/java/hbz/limetrans/function/MemberLocal.java
@@ -1,9 +1,10 @@
 package hbz.limetrans.function;
 
-import org.metafacture.metafix.FixConditional;
 import org.metafacture.metafix.Metafix;
 import org.metafacture.metafix.Record;
 import org.metafacture.metafix.api.FixPredicate;
+import org.metafacture.metafix.conditional.AnyEqual;
+import org.metafacture.metafix.conditional.Exists;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,17 +18,16 @@ public class MemberLocal implements FixPredicate {
     }
 
     @Override
-    @SuppressWarnings("removal")
     public boolean test(final Metafix aMetafix, final Record aRecord, final List<String> aParams, final Map<String, String> aOptions) {
         final List<String> params = new ArrayList<>();
         params.add(aParams.get(0) + "." + SUBFIELD);
 
-        if (!FixConditional.exists.test(aMetafix, aRecord, params, null)) {
+        if (!new Exists().test(aMetafix, aRecord, params, null)) {
             return true;
         }
         else {
             params.add(aMetafix.getVars().get("member"));
-            return FixConditional.any_equal.test(aMetafix, aRecord, params, null);
+            return new AnyEqual().test(aMetafix, aRecord, params, null);
         }
     }
 
